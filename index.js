df2271da214bb411f411eecc305967b8ed70f561
Oscar5323 <jr.mercado2001@gmail.com>
2023-07-17 19:54:22 -0500

    added the promt

---
+++ index.js
@@ -0,0 +1,44 @@
+const inquire= require('inquirer')
+const fs = require('fs')
+
+
+
+
+inquire
+    .prompt([
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+        {
+            type:'',
+            name:'',
+            message:'',
+        },
+    ])
